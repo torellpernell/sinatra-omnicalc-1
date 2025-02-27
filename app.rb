68cb583c35b281c31c33ee4b636ac5ae1a68626d
Torell T. Pernell <torellpernell@yahoo.com>
2025-02-27 07:20:31 +0000

    another commit

--- app.rb
+++ app.rb
@@ -2,12 +2,41 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:"square/new")
+end
 
+get("/square/new") do
+  erb(:"square/new")
+end
 
-erb(:"square/new")
+get("/square/results") do
+   @number = params.fetch("number")
 
+  erb(:"square/results")
+end
+
+get("/square_root/new") do
+  
+  erb(:"square_root/new")
+end
+
+get("/square_root/results") do
+  @number = params.fetch("user_number")
+  erb(:"square_root/results")
+end
+
+get("/random/new") do
+  erb(:"random/new")
+end
+
+get("/random/results") do
+  erb(:"random/results")
+end
+
+get("/payment/new") do
+  erb(:"payment/new")
+end
+
+get("/payment/results") do
+  erb(:"payment/results")
 end
